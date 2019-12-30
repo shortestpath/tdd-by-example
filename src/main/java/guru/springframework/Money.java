39d4a479e1413531febc68023224d54590f938a8
Alex Chang <shortestpath@gmail.com>
2019-12-30 08:06:16 -0800

    finished section 3 - tdd by example

---
+++ src/main/java/guru/springframework/Money.java
@@ -0,0 +1,50 @@
+package guru.springframework;
+
+public class Money implements Expression {
+    final int amount;
+    private final String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    String currency() {
+        return currency;
+    }
+
+    static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    public boolean equals(Object object) {
+        Money money = (Money)object;
+        return amount == money.amount &&
+                this.currency == money.currency;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+
+    public Expression times(int multiplier) {
+        return new Money(amount * multiplier, this.currency);
+    }
+
+    public Expression plus(Expression addend) {
+        return new Sum(this, addend);
+    }
+
+    @Override
+    public Money reduce(Bank bank, String to) {
+        return new Money(amount / bank.rate(this.currency, to), to);
+    }
+}
